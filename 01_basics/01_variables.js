00b38142a812a194a26d6068e485787efd14c07a
shoaibwebdev-hub <shoaibalam.dev@gmail.com>
2025-12-08 20:35:40 +0500

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1234
+let accountEmail = "shoaibawan@gmail.com"
+var accountPassword = "1122"
+accountCity = "Islamabad"
+let accountState ;
+
+
+accountEmail="hr@gmail.com"
+accountPassword="212121"
+accountCity="Lahore"
+
+console.log(accountId);
+
+// prefere not to use var because of issue in block scope functional scope
+
+
+console.table([accountId, accountEmail,accountPassword, accountState])
+
+
