08c367f7a099e90ec72cc171b4461f20a41faa6c
shoaibwebdev-hub <shoaibalam.dev@gmail.com>
2025-12-09 13:50:48 +0500

    Add datatypes and ConversionOp

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score =  "shoaib"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 , false = 0
+
+let someNumber = 44
+
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
